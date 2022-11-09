254c8bf6f01d83d86255f921567ab62873c36f8e
soroushes1 <esmailian.soroush246@gmail.com>
2022-11-09 18:54:28 +0330

    structure created

---
+++ src/pages/MusicPlay.tsx
@@ -0,0 +1,6 @@
+const MusicPlay = ()=>{
+    return(
+        <audio controls src="https://dl.naslemusic.com/music/1401/01/Shadmehr%20Aghili%20-%20Bi%20Ehsas%20(320-Naslemusic).mp3"></audio>
+    )
+}
+export default MusicPlay ;
